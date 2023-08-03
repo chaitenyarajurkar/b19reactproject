235428954a6b96e1e8ca2090e993c1003e92f6fa
Chaitenya-rajurkar <rajuchetan547@gmail.com>
2023-08-03 22:05:14 +0530

    day9

---
+++ day9/src/common.js
@@ -0,0 +1,11 @@
+const name = "code first academy";
+
+const address = "Pune";
+
+const callingData =()=>{
+      return "hello world"
+}
+// export default name;
+
+
+export {name , address ,callingData};
