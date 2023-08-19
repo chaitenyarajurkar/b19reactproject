6d759cca155254c6f57cc6feaaea7fb1319bd20a
Chaitenya-rajurkar <rajuchetan547@gmail.com>
2023-08-19 21:29:54 +0530

    get_query_param_try_catch_block

---
+++ day19_trycatch_queryparram/src/Components/Post.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+// get
+// post 
+// put
+// patch 
+// delete
+
+// baseurl ="http://jsonplaceholder.typicode.com"
+//endpoint = "/posts"
+//query parameters = "?postId=1"
+const Post = () => {
+    const [postData,setPostData] = useState([]);
+    
+    const getDataFromServer =async()=>{
+         try {
+            //  request api via query parameter
+           const response = await axios.get("http://jsonplaceholder.typicode.com/comments",{params:{postId:6}});
+           console.log(response.data);
+           setPostData(response.data);
+
+         } catch (error) {
+            console.log(error)
+            alert("there is some");
+         }
+        
+        
+         
+    }
+
+    return (
+        <div>
+            <button onClick={()=>getDataFromServer()}>Get Articles</button>
+            <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Body</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    {postData.map((val,index)=>{
+        return (
+
+    <tr key={index}>
+      <th scope="row">{val.id}</th>
+      <td>{val.name}</td>
+      <td>{val.email}</td>
+      <td>@{val.body}</td>
+    </tr>
+        )
+    })}
+   
+  </tbody>
+</table>
+               
+        </div>
+    );
+};
+
+export default Post;
