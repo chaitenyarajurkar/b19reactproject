155824441de3ee030e2f59fb89f919fb5cc2763b
Chaitenya-rajurkar <rajuchetan547@gmail.com>
2023-08-31 21:41:31 +0530

    ecom project add

---
+++ ProjectEccom_Context_api/eccom/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Component/Navbar/Navbar';
+
+function App() {
+  return (
+   <div className='constainer-fluid'>
+
+       <Navbar></Navbar>
+   </div>
+  );
+}
+
+export default App;
