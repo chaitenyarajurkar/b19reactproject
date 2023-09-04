15996a0b1b2455eb406ab8c678595523e6e60e9e
Chaitenya-rajurkar <rajuchetan547@gmail.com>
2023-09-04 21:45:36 +0530

    project work

--- ProjectEccom_Context_api/eccom/src/Component/Pages/Mobile/index.js
+++ ProjectEccom_Context_api/eccom/src/Component/Pages/Mobile/index.js
@@ -21,7 +21,26 @@ const Mobile = () => {
     },[])
     return (
         <div>
-            <h1>These are Mobiles</h1>
+            <h1 className='text-center'>Mobiles :</h1>
+            <div className='row'>
+
+          
+              {
+                mobileData?.length > 0 && mobileData.map((item)=>{
+
+                    return(
+                        <div className="card col-4 m-2 " style={{ width: "18rem" ,height:"200" }}>
+                        <img className="card-img-top" src={item.productImageUrl} alt="Card  cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">{item.productShortName}</h5>
+                            <p className="card-text">{item.productDescription}</p>
+                            <a href="/" className="btn btn-primary">Add To Cart</a>
+                        </div>
+                    </div>
+                    )
+                })
+              }
+             </div>
         </div>
     );
 };
